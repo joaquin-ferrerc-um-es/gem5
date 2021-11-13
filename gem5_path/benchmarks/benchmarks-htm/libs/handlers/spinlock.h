c5551df22eb9b82abddfb7be1effc65c9ad4fd9c
Rubén Titos <rtitos@um.es>
2021-11-13 01:13:13 +0100

    ext: HTM library arm64 implementation of ticket spinlock.
    Taken from the linux kernel, it is the same implementation
    used by the vmlinux.arm64 in gem5 public resources for FS.
    This implementation replaces the "naïve" implementation
    used so far, based on Tom Hayes' article on TME support
    on gem5, which uses C++ atomics to implement busy wait.
    The resulting code does not employ sev/wfe instructions
    and is thus subject to livelocks in high contention
    scenarios due to repeatedly failed LLSC because of other
    threads' store conditionals invalidating the monitor.
    
    The new implementation is copied from <raw_spin_lock> in
    the Linux kernel, objdump -d vmlinux.arm64 from here
    http://dist.gem5.org/
    dist/v21-1/arm/aarch-system-20210904.tar.bz2
    
    ffffff800872e700:       prfm    pstl1strm, [x19]
    ffffff800872e704:       ldaxr   w0, [x19]
    ffffff800872e708:       add     w1, w0, #0x10, lsl #12
    ffffff800872e70c:       stxr    w2, w1, [x19]
    ffffff800872e710:       cbnz    w2, ffffff800872e704
    ffffff800872e714:       eor     w1, w0, w0, ror #16
    ffffff800872e718:       cbz     w1, ffffff800872e730
    ffffff800872e71c:       sevl
    ffffff800872e720:       wfe
    ffffff800872e724:       ldaxrh  w2, [x19]
    ffffff800872e728:       eor     w1, w2, w0, lsr #16
    ffffff800872e72c:       cbnz    w1, ffffff800872e720

--- gem5_path/benchmarks/benchmarks-htm/libs/handlers/spinlock.h
+++ gem5_path/benchmarks/benchmarks-htm/libs/handlers/spinlock.h
@@ -7,6 +7,10 @@
 
 #if defined (AARCH64)
 
+#if defined (AARCH64_NAIVE_SPINLOCK)
+
+// Naive spinlock implementation from:
+//  https://community.arm.com/arm-research/b/articles/posts/arms-transactional-memory-extension-support-
 #include <stdatomic.h>
 
 typedef atomic_int lock_t;
@@ -48,13 +52,92 @@ static inline void spinlock_lock()
     }
     while (atomic_exchange_explicit(&fallbackLock.lock, 1,
                                     memory_order_acquire));
-}
 
+    // This will generate a full memory barrier, e.g. DMB SY
+    atomic_thread_fence(memory_order_seq_cst);
+}
 
 static inline void spinlock_unlock()
 {
     atomic_store_explicit(&fallbackLock.lock, 0, memory_order_release);
 }
+
+#else // ARM64_NAIVE SPINLOCK
+
+// Ticket spinlock implementation found in the linux kernel, adapted from
+// https://github.com/siemens/jailhouse/blob/master/hypervisor/arch/arm64/include/asm/spinlock.h
+
+#define TICKET_SHIFT	16
+
+typedef struct {
+	uint16_t owner;
+	uint16_t next;
+} spinlock_t __attribute__ ((aligned (CACHE_LINE_SIZE_BYTES)));
+
+spinlock_t fallbackLock;
+
+static inline void * spinlock_getAddress()
+{
+    return (void *) &fallbackLock;
+}
+static inline void spinlock_init()
+{
+    fallbackLock.owner = 0;
+    fallbackLock.next = 0;
+}
+static inline long spinlock_isLocked()
+{
+    return fallbackLock.owner != fallbackLock.next;
+}
+
+static inline void spinlock_whileIsLocked()
+{
+  while (spinlock_isLocked()) {
+      // __yield();
+  }
+}
+
+static inline void spinlock_lock()
+{
+	unsigned int tmp;
+	spinlock_t lockval, newval;
+
+        __asm__ __volatile__(
+	/* Atomically increment the next ticket. */
+"	prfm	pstl1strm, %3\n"
+"1:	ldaxr	%w0, %3\n"
+"	add	%w1, %w0, %w5\n"
+"	stxr	%w2, %w1, %3\n"
+"	cbnz	%w2, 1b\n"
+	/* Did we get the lock? */
+"	eor	%w1, %w0, %w0, ror #16\n"
+"	cbz	%w1, 3f\n"
+	/*
+	 * No: spin on the owner. Send a local event to avoid missing an
+	 * unlock before the exclusive load.
+	 */
+"	sevl\n"
+"2:	wfe\n"
+"	ldaxrh	%w2, %4\n"
+"	eor	%w1, %w2, %w0, lsr #16\n"
+"	cbnz	%w1, 2b\n"
+	/* We got the lock. Critical section starts here. */
+"3:"
+	: "=&r" (lockval), "=&r" (newval), "=&r" (tmp), "+Q" (fallbackLock)
+	: "Q" (fallbackLock.owner), "I" (1 << TICKET_SHIFT)
+	: "memory");
+}
+
+static inline void spinlock_unlock()
+{
+        __asm__ __volatile__(
+"	stlrh	%w1, %0\n"
+	: "=Q" (fallbackLock.owner)
+	: "r" (fallbackLock.owner + 1)
+	: "memory");
+}
+
+#endif
 
 #elif defined (X86)
 

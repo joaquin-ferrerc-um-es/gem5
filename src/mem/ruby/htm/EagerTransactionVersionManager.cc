e300065815a1aa1a0d05b5404bbb9d85629c75b3
Rubén Titos <rtitos@um.es>
2021-11-25 00:11:11 +0100

    mem-ruby: HTM support for eager versioning (log-based HTM)
    Added class to encapsulate logging functionality
    
    Change-Id: I4bcb4a930130226057a286aae3e9431a8c33ac69

---
+++ src/mem/ruby/htm/EagerTransactionVersionManager.cc
@@ -0,0 +1,152 @@
+/*
+  Copyright (C) 2016-2021 Rubén Titos <rtitos@um.es>
+  Universidad de Murcia
+
+  GPLv2, see file LICENSE.
+*/
+
+#include "mem/ruby/htm/EagerTransactionVersionManager.hh"
+
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+#include "debug/RubyHTMlog.hh"
+#include "debug/RubyHTMverbose.hh"
+#include "mem/packet.hh"
+#include "mem/ruby/common/DataBlock.hh"
+#include "mem/ruby/htm/TransactionInterfaceManager.hh"
+#include "mem/ruby/htm/logtm.h"
+#include "mem/ruby/system/Sequencer.hh"
+
+#define CLASS_NS EagerTransactionVersionManager::
+
+namespace gem5
+{
+namespace ruby
+{
+
+
+CLASS_NS
+EagerTransactionVersionManager(TransactionInterfaceManager *xact_mgr,
+                              int version, CacheMemory* dataCache_ptr) {
+    m_version = version;
+    m_xact_mgr = xact_mgr;
+    m_dataCache_ptr = dataCache_ptr;
+}
+
+CLASS_NS ~EagerTransactionVersionManager() {
+}
+
+void
+CLASS_NS beginTransaction(int thread)
+{
+    assert(m_logNumEntries == 0);
+    assert(m_initStatus == LogInitStatus::Ready);
+    assert(!m_logTLB.empty());
+}
+
+void
+CLASS_NS restartTransaction(int thread){
+    m_logNumEntries = 0;
+}
+
+
+void
+CLASS_NS commitTransaction(int thread)
+{
+    m_logNumEntries = 0;
+}
+
+bool
+CLASS_NS isAccessToLog(Addr addr) const
+{
+    if (m_initStatus == LogInitStatus::Invalid)
+        return false;
+    return (addr >= m_logBaseVAddr &&
+            addr  < (m_logBaseVAddr + MAX_LOG_SIZE_BYTES));
+}
+
+void
+CLASS_NS setupLogTranslation(Addr vaddr, Addr paddr)
+{
+    if (m_initStatus == LogInitStatus::BaseAddress) {
+        assert(m_logTLB.empty());
+        // First translation must be for base address
+        assert(m_logBaseVAddr == vaddr);
+    } else {
+        assert(m_initStatus == LogInitStatus::V2PTranslations);
+    }
+    m_initStatus = LogInitStatus::V2PTranslations;
+    assert(m_logTLB.find(vaddr) == m_logTLB.end());
+    m_logTLB[vaddr] = paddr;
+    DPRINTF(RubyHTMlog,
+            "Setting up log TLB vaddr %#x paddr %#x \n",
+            vaddr, paddr);
+    if (vaddr + LOG_PAGE_SIZE_BYTES ==
+        m_logBaseVAddr + MAX_LOG_SIZE_BYTES) {
+        m_initStatus = LogInitStatus::Ready;
+        DPRINTF(RubyHTMlog,
+                "Done setting log translations. Log ready! "
+                "Log max size is %ld bytes\n",
+                MAX_LOG_SIZE_BYTES);
+    }
+}
+
+
+void
+CLASS_NS setLogBaseVirtualAddress(Addr addr)
+{
+    if (m_initStatus == LogInitStatus::Invalid) {
+        m_initStatus = LogInitStatus::BaseAddress;
+        m_logBaseVAddr = addr;
+        DPRINTF(RubyHTMlog,
+                "Setting up log base vaddr %#x \n",
+                addr);
+    } else {
+        assert(m_logBaseVAddr == addr);
+    }
+}
+
+
+Addr
+CLASS_NS computeLogDataPointer(int numEntries) const {
+    assert(numEntries >= 0 &&
+           numEntries < MAX_LOG_SIZE_PAGES * LOG_ENTRIES_PER_PAGE);
+    return (Addr)logtm_compute_data_log_pointer(m_logBaseVAddr,
+                                                numEntries);
+}
+Addr
+CLASS_NS translateLogAddress(Addr vaddr) const {
+    Addr vpageAddr = vaddr & PAGE_MASK;
+    Addr pageOffset = vaddr & ~PAGE_MASK;
+    std::map<Addr,Addr>::const_iterator it =
+        m_logTLB.find(vpageAddr);
+    assert(it != m_logTLB.end());
+    Addr ppageAddr = it->second;
+    return ppageAddr | pageOffset;
+}
+
+Addr
+CLASS_NS addLogEntry(Addr storeAddr)
+{
+    // storeAddr: target block vaddr of transactional store
+    // TODO: Keep track of logged virtual addresses??
+    // Should never be called unless we have set the log base
+    assert(m_initStatus = LogInitStatus::Ready);
+
+    // Returns vaddr of log entry to be used for logging this store,
+    // according to current number of entries, and increments number
+    // of entries
+    Addr vaddr = computeLogDataPointer(m_logNumEntries);
+    ++m_logNumEntries;
+    Addr paddr = translateLogAddress(vaddr);
+    DPRINTF(RubyHTMlog,
+            "Logging store to paddr %#x - log vaddr %#x log paddr %#x \n",
+            storeAddr, vaddr, paddr);
+
+    return paddr;
+}
+
+} // namespace ruby
+} // namespace gem5
---
+++ src/mem/ruby/htm/EagerTransactionVersionManager.hh
@@ -0,0 +1,75 @@
+/*
+  Copyright (C) 2016-2021 Rubén Titos <rtitos@um.es>
+  Universidad de Murcia
+
+  GPLv2, see file LICENSE.
+*/
+
+#ifndef __MEM_RUBY_HTM_EAGERTRANSACTIONVERSIONMANAGER_HH__
+#define __MEM_RUBY_HTM_EAGERTRANSACTIONVERSIONMANAGER_HH__
+
+#include <map>
+#include <vector>
+
+#include "mem/packet.hh"
+#include "mem/request.hh"
+#include "mem/ruby/common/DataBlock.hh"
+#include "mem/ruby/structures/CacheMemory.hh"
+
+namespace gem5
+{
+namespace ruby
+{
+
+using namespace std;
+
+class TransactionInterfaceManager;
+
+class EagerTransactionVersionManager
+{
+public:
+  EagerTransactionVersionManager(TransactionInterfaceManager *xact_mgr,
+                                int version,
+                                CacheMemory* dataCache_ptr);
+  ~EagerTransactionVersionManager();
+
+  void beginTransaction(int thread);
+  void restartTransaction(int thread);
+  void commitTransaction(int thread);
+  bool isReady() const { return m_initStatus == LogInitStatus::Ready; };
+  bool isAccessToLog(Addr addr) const;
+  void setupLogTranslation(Addr vaddr, Addr paddr);
+  void setLogBaseVirtualAddress(Addr addr);
+  Addr getLogBaseVirtualAddress() const {
+      assert(m_initStatus >= LogInitStatus::BaseAddress);
+      return m_logBaseVAddr; };
+  Addr addLogEntry(Addr addr);
+  int getLogNumEntries() const {
+      assert(m_initStatus == LogInitStatus::Ready);
+      return m_logNumEntries;
+  };
+
+private:
+  enum LogInitStatus
+  {
+      Invalid,
+      BaseAddress,
+      V2PTranslations,
+      Ready
+  };
+  Addr computeLogDataPointer(int numEntries) const;
+  Addr translateLogAddress(Addr vaddr) const;
+  TransactionInterfaceManager *m_xact_mgr;
+  int m_version;
+  CacheMemory *m_dataCache_ptr;
+  LogInitStatus m_initStatus = LogInitStatus::Invalid;
+  Addr m_logBaseVAddr;
+  std::map<Addr, Addr> m_logTLB;
+  int m_logNumEntries = 0;
+};
+
+} // namespace ruby
+} // namespace gem5
+
+#endif
+

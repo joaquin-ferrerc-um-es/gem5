1a6db528ce02932a8fc54af1d0895c31699301d1
Rubén Titos <rtitos@um.es>
2021-12-18 13:57:32 +0100

    ext: HTM Wset eviction options in simulation config Python scripts

--- gem5_path/scripts/htm.py
+++ gem5_path/scripts/htm.py
@@ -17,6 +17,8 @@ htm_lazy_arbitration           = HtmOption("htm_lazy_arbitration",              
 htm_lazy_validated_conf_res    = HtmOption("htm_lazy_validated_conf_res",       "LzCR",     "lazy-validated-conf-res",           False, True , True  )
 htm_allow_read_set_l0_evictions= HtmOption("htm_allow_read_set_l0_evictions",   "RSL0Ev",   "allow-read-set-l0-cache-evictions", True,  True , True  )
 htm_allow_read_set_l1_evictions= HtmOption("htm_allow_read_set_l1_evictions",   "RSL1Ev",   "allow-read-set-l1-cache-evictions", True,  True , True  )
+htm_allow_write_set_l0_evictions= HtmOption("htm_allow_write_set_l0_evictions", "WSL0Ev",   "allow-write-set-l0-cache-evictions", True,  True , True  )
+htm_allow_write_set_l1_evictions= HtmOption("htm_allow_write_set_l1_evictions", "WSL1Ev",   "allow-write-set-l1-cache-evictions", True,  True , True  )
 htm_precise_read_set_tracking  = HtmOption("htm_precise_read_set_tracking",     "RSPrec",   "precise-read-set-tracking",         True,  True , True  )
 htm_trans_aware_l0_replacements= HtmOption("htm_trans_aware_l0_replacements",   "L0Repl",   "trans-aware-l0-replacements",       True,  True , True  )
 htm_trans_aware_l1_replacements= HtmOption("htm_trans_aware_l1_replacements",   "L1Repl",   "trans-aware-l1-replacements",       True,  True , True  )
@@ -40,6 +42,8 @@ htm_config_options.append(htm_lazy_arbitration)
 htm_config_options.append(htm_lazy_validated_conf_res)
 htm_config_options.append(htm_allow_read_set_l0_evictions)
 htm_config_options.append(htm_allow_read_set_l1_evictions)
+htm_config_options.append(htm_allow_write_set_l0_evictions)
+htm_config_options.append(htm_allow_write_set_l1_evictions)
 htm_config_options.append(htm_precise_read_set_tracking)
 htm_config_options.append(htm_trans_aware_l0_replacements)
 htm_config_options.append(htm_trans_aware_l1_replacements)
@@ -67,6 +71,8 @@ cfg1_base[htm_lazy_arbitration]=None
 cfg1_base[htm_lazy_validated_conf_res]=None
 cfg1_base[htm_allow_read_set_l0_evictions]=False
 cfg1_base[htm_allow_read_set_l1_evictions]=False
+cfg1_base[htm_allow_write_set_l0_evictions]=False
+cfg1_base[htm_allow_write_set_l1_evictions]=False
 cfg1_base[htm_precise_read_set_tracking]=False
 cfg1_base[htm_trans_aware_l0_replacements]=False
 cfg1_base[htm_trans_aware_l1_replacements]=False
@@ -86,11 +92,8 @@ cfg1_precise[htm_precise_read_set_tracking]=True
 cfg1_reqstalls = collections.OrderedDict(cfg1_base)
 cfg1_reqstalls[htm_conflict_resolution]='requester_stalls_cda_hybrid'
 
-cfg1_rset_l0_evict = collections.OrderedDict(cfg1_base)
-cfg1_rset_l0_evict[htm_allow_read_set_l0_evictions]=True
-
-cfg1_replace_nontx = collections.OrderedDict(cfg1_base)
-cfg1_replace_nontx[htm_trans_aware_l0_replacements]=True
+cfg1_l0rsetevict = collections.OrderedDict(cfg1_base)
+cfg1_l0rsetevict[htm_allow_read_set_l0_evictions]=True
 
 # Now choose a different baseline in which we change based on the
 # results from the first round of parameter exploration
@@ -99,6 +102,7 @@ cfg1_replace_nontx[htm_trans_aware_l0_replacements]=True
 cfg2_base = collections.OrderedDict(cfg1_base)
 cfg2_base[htm_trans_aware_l0_replacements]=True
 cfg2_base[htm_l0_downgrade_on_l1_gets]=True
+#cfg2_base[htm_value_checker]=True
 
 cfg2_precise = collections.OrderedDict(cfg2_base)
 cfg2_precise[htm_precise_read_set_tracking]=True
@@ -108,6 +112,8 @@ cfg2_reqstalls[htm_conflict_resolution]='requester_stalls_cda_hybrid'
 
 cfg2_eagervm = collections.OrderedDict(cfg2_base)
 cfg2_eagervm[htm_lazy_vm]=False
+cfg2_eagervm[htm_allow_write_set_l0_evictions]=True
+cfg2_eagervm[htm_allow_write_set_l1_evictions]=True
 
 cfg2_eagervm_l0rsetevict = collections.OrderedDict(cfg2_eagervm)
 cfg2_eagervm_l0rsetevict[htm_allow_read_set_l0_evictions]=True
@@ -120,14 +126,24 @@ cfg2_eagervm_l0rsetevict_pf_cdah[htm_conflict_resolution]='requester_stalls_cda_
 
 cfg2_eagervm_l1rsetevict_pf_cdah = collections.OrderedDict(cfg2_eagervm_l0rsetevict_pf_cdah)
 cfg2_eagervm_l1rsetevict_pf_cdah[htm_allow_read_set_l1_evictions]=True
-cfg2_eagervm_l1rsetevict_pf_cdah[htm_isolation_checker]=True
 cfg2_eagervm_l1rsetevict_pf_cdah[htm_value_checker]=True
 
 cfg2_eagervm_l1rsetevict_cdah = collections.OrderedDict(cfg2_eagervm_l1rsetevict_pf_cdah)
 cfg2_eagervm_l1rsetevict_cdah[htm_heap_prefault]=False
-cfg2_eagervm_l1rsetevict_cdah[htm_value_checker]=True
-cfg2_eagervm_l1rsetevict_cdah[htm_isolation_checker]=True
 
+cfg2_lazycd_l0rsetevict = collections.OrderedDict(cfg2_base)
+cfg2_lazycd_l0rsetevict[htm_eager_cd]=False
+cfg2_lazycd_l0rsetevict[htm_allow_read_set_l0_evictions]=True
+cfg2_lazycd_l0rsetevict[htm_conflict_resolution]='requester_wins'
+
+cfg2_lazycd_l0rsetevict_pf = collections.OrderedDict(cfg2_lazycd_l0rsetevict)
+cfg2_lazycd_l0rsetevict_pf[htm_heap_prefault]=True
+
+cfg2_lazycd_l1rsetevict_pf = collections.OrderedDict(cfg2_lazycd_l0rsetevict_pf)
+cfg2_lazycd_l1rsetevict_pf[htm_allow_read_set_l1_evictions]=True
+
+cfg2_eagercd_l1rsetevict_pf = collections.OrderedDict(cfg2_lazycd_l1rsetevict_pf)
+cfg2_eagercd_l1rsetevict_pf[htm_eager_cd]=True
 
 # No need to try every combination, but rather "guide" the search...
 

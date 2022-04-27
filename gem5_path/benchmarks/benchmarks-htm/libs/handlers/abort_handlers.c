0818a5fd1fbfdd4c8e5d7eedba150563b00b1b00
Rubén Titos <rtitos@um.es>
2022-04-27 20:31:58 +0200

    ext: Avoid seg fault when running binaries in real hardware
    Change-Id: I112747f6a77c9f5262b37f966f85daaa2a35d2ec

--- gem5_path/benchmarks/benchmarks-htm/libs/handlers/abort_handlers.c
+++ gem5_path/benchmarks/benchmarks-htm/libs/handlers/abort_handlers.c
@@ -48,7 +48,9 @@ void deleteGlobals () {
     // Calling simSetLogBase with a NULL ptr when the log is ready is
     // used to shutdown the log in all CPUs to stop stop monitoring
     // the virtual addresses allocated to the log (sanity checks)
-    simSetLogBase(NULL);
+    if (inSimulator()) {
+        simSetLogBase(NULL);
+    }
 }
 
 // "Touch" function in STAMPs memory allocator

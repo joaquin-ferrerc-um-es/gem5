b91af908d1357fd5f5690f4aba4dcb9750ebb39a
Rubén Titos <rtitos@um.es>
2023-02-21 10:49:59 +0100

    ext: Add power coordinate to results-scripts
    Change-Id: I0a5f33b0c6b9d2e7692629246f9b5ca1079a9035

--- gem5_path/scripts/results-scripts-gem5/program/src/main/scala/repscr/gem5/Gem5Coords.scala
+++ gem5_path/scripts/results-scripts-gem5/program/src/main/scala/repscr/gem5/Gem5Coords.scala
@@ -129,6 +129,7 @@ object Gem5Coords extends PlotCoordinates[Gem5DataPoint] {
       case (".htm.sgl", _, _, _, _, _, _, _, _, _, _) => "locks"
       case (".htm.fallbacklock", false, _, _, _, _, true, "requester_wins", _, true, _) => "base_nopf"
       case (".htm.fallbacklock", true, false, _, _, _, true, "requester_wins", _, true, _) => "base"
+      case (".htm.powertm",      true, true,  _, _, _, true, "power_tm",       _, true, _) => "power"
 
       case (".htm.fallbacklock", true, true, false, _, _, true, "requester_wins", _, true, _) => "l2rs"
       case (".htm.fallbacklock", true, true, true, false, false, true, "requester_wins", _, true, _) => "l3rs"
@@ -149,6 +150,7 @@ object Gem5Coords extends PlotCoordinates[Gem5DataPoint] {
     isConfig = true,
     ordering = dynamicOrdering("locks", "base_nopf",
       "base", "l2rs", "l3rs", "lxrs",
+      "power",
       "l3rs_l1rpl", "l3rs_l1rpl_reqstallb", "l3rs_l1rpl_reqstallh",
       "l3rs_l1rpl_reqstallh_precrs", "l3rs_l1rpl_lazycd"
 

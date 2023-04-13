7c2d274be7fb40aab6e0a65639ebb32e7b67d4fd
Ricardo Fernández Pascual <ricardof@um.es>
2023-04-13 13:18:26 +0200

    ext: Some cleanups to htmrt

---
+++ gem5_path/benchmarks/libs/htmrt/htmrt.h
@@ -0,0 +1,15 @@
+
+#ifndef HTMRT_H
+#define HTMRT_H 1
+
+#include "thread_context.h"
+
+void initTransactionsGlobals(int num_threads);
+void deleteTransactionsGlobals(void);
+
+void beginTransaction(long tag, _tm_thread_context_t *thread_context);
+void commitTransaction(long tag, _tm_thread_context_t *thread_context);
+void cancelTransactionWithAbortCode(long abort_code);
+void cancelTransaction();
+
+#endif

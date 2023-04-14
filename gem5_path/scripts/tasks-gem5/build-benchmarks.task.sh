39729ebc3936f3e46ebd7d49b2a517e512c7a526
Ricardo Fernández Pascual <ricardof@um.es>
2023-04-14 12:19:15 +0200

    ext: Initial support for compiling HTMBench.
    Change-Id: I61c880d1ee3b455e5632e88a9a0e4ff11b85451b

--- gem5_path/scripts/tasks-gem5/build-benchmarks.task.sh
+++ gem5_path/scripts/tasks-gem5/build-benchmarks.task.sh
@@ -19,6 +19,10 @@ BENCHMARKS_STAMP_SELECTED=(
     "yada"
 )
 
+BENCHMARKS_HTMBENCH_SELECTED=(
+    "berkely-db"
+)
+
 task_build-benchmarks() {
     local -a archs=("${ENABLED_ARCHITECTURES[@]}")
     options="$(simpler_getopt "architecture:" "$@")"
@@ -49,12 +53,24 @@ build_benchmarks() {
         echo "$(color yellow "Skipping build of test benchmark (sumarray) because it is not yet supported for '$arch'. TODO: fix this")"
     fi
 
-    if [ "${BENCHMARKS_STAMP_ENABLED[$arch]}" = "yes" ] ; then
+    if [[ "${BENCHMARKS_STAMP_ENABLED[$arch]}" = "yes" || "${BENCHMARKS_STAMP_ENABLED[$arch]}" = "yes-native" ]] ; then
         build_benchmarks_stamp "$arch"
+    elif [[ "${BENCHMARKS_STAMP_ENABLED[$arch]}" = "yes-virtual" ]] ; then
+        echo "$(color green "STAMP benchmarks will not be built because they are built directly in the image for $arch.")"
     elif [[ "${BENCHMARKS_STAMP_ENABLED[$arch]}" = "no" ]] ; then
         echo "$(color green "STAMP benchmarks disabled for $arch.")"
     else
         error_and_exit "Invalid value for BENCHMARKS_STAMP_ENABLED[$arch] (${BENCHMARKS_STAMP_ENABLED[$arch]})"
+    fi
+
+    if [[ "${BENCHMARKS_HTMBENCH_ENABLED[$arch]}" = "yes-native" ]] ; then
+        build_benchmarks_htmbench "$arch"
+    elif [[ "${BENCHMARKS_HTMBENCH_ENABLED[$arch]}" = "yes-virtual" ]] ; then
+        echo "$(color green "HTMBENCH benchmarks will not be built because they are built directly in the image for $arch.")"
+    elif [[ "${BENCHMARKS_HTMBENCH_ENABLED[$arch]}" = "no" ]] ; then
+        echo "$(color green "HTMBENCH benchmarks disabled for $arch.")"
+    else
+        error_and_exit "Invalid value for BENCHMARKS_HTMBENCH_ENABLED[$arch] (${BENCHMARKS_HTMBENCH_ENABLED[$arch]})"
     fi
 
     if [[ "${BENCHMARKS_PARSEC_ENABLED[$arch]}" = "yes-native" ]] ; then
@@ -106,7 +122,7 @@ build_benchmarks_stamp() {
             else
                 (
                     echo "$(color green "Build $b.$a.$s")"
-                    cd "$(absolute_path "$BENCHMARKS_HTM_STAMP/$b")"
+                    cd "$(absolute_path "$BENCHMARKS_HTM_STAMP_DIR/$b")"
                     make -j $(get_num_threads_for_building) -f "Makefile.$s" "ARCH=$arch"
                 )
             fi
@@ -115,12 +131,49 @@ build_benchmarks_stamp() {
 }
 
 check_stamp_gem5_directory_links() {
-    if [[ ! -d "$(absolute_path "$BENCHMARKS_HTM_STAMP")" || ! -L "${GEM5_ROOT}/${BENCHMARKS_HTM_STAMP}" ]] ; then
-        error_and_exit "Stamp directory symlink '$(absolute_path "$BENCHMARKS_HTM_STAMP")' not found. Clone the repository in a directory out of ${GEM5_ROOT} and create a symbolic link to it in '$(dirname "$(absolute_path "$BENCHMARKS_HTM_STAMP")")'."
+    if [[ ! -d "$(absolute_path "$BENCHMARKS_HTM_STAMP_DIR")" || ! -L "${GEM5_ROOT}/${BENCHMARKS_HTM_STAMP_DIR}" ]] ; then
+        error_and_exit "Stamp directory symlink '$(absolute_path "$BENCHMARKS_HTM_STAMP_DIR")' not found. Clone the repository in a directory out of ${GEM5_ROOT} and create a symbolic link to it in '$(dirname "$(absolute_path "$BENCHMARKS_HTM_STAMP_DIR")")'."
     fi
 
-    if [[ ! -d "$(absolute_path "$BENCHMARKS_HTM_STAMP")/gem5" ]] ; then
-        ln -s "$GEM5_ROOT" "$(absolute_path "$BENCHMARKS_HTM_STAMP")/gem5"
+    if [[ ! -d "$(absolute_path "$BENCHMARKS_HTM_STAMP_DIR")/gem5" ]] ; then
+        ln -s "$GEM5_ROOT" "$(absolute_path "$BENCHMARKS_HTM_STAMP_DIR")/gem5"
+    fi
+}
+
+build_benchmarks_htmbench() {
+    local arch="$1"
+
+    echo "$(color green "Building HTMBench benchmarks for $arch")"
+
+    if [[ "$arch" = "x86_64" ]] ; then
+        export X86_CROSS_GCC_PREFIX="${BENCHMARKS_ARCH_COMPILER_PREFIX[$arch]}"
+    elif [[ "$arch" = "aarch64" ]] ; then
+        export AARCH64_CROSS_GCC_PREFIX="${BENCHMARKS_ARCH_COMPILER_PREFIX[$arch]}"
+    else
+        error_and_exit "Architecture $arch not supported for htmbench"
+    fi
+    
+    check_htmbench_gem5_directory_links
+    
+    for b in "${BENCHMARKS_HTMBENCH_SELECTED[@]}" ; do
+        for h in "${BENCHMARKS_HTMBENCH_FLAVOURS[@]}" ; do
+            (
+                echo "$(color green "Build $b ARCH=$a HANDLER=$h")"
+                cd "$(absolute_path "$BENCHMARKS_HTM_HTMBENCH_DIR/benchmark/$b")"
+                make -j $(get_num_threads_for_building) "ARCH=$arch" "HANDLER=$h"
+            )
+        done
+    done
+}
+
+check_htmbench_gem5_directory_links() {
+    if [[ ! -d "$(absolute_path "$BENCHMARKS_HTM_HTMBENCH_DIR")" || ! -L "${GEM5_ROOT}/${BENCHMARKS_HTM_HTMBENCH_DIR}" ]] ; then
+        error_and_exit "HTMBench directory symlink '$(absolute_path "$BENCHMARKS_HTM_HTMBENCH_DIR")' not found. Clone the repository in a directory out of ${GEM5_ROOT} and create a symbolic link to it in '$(dirname "$(absolute_path "$BENCHMARKS_HTM_HTMBENCH_DIR")")'."
+    fi
+
+    if [[ ! -d "$(absolute_path "$BENCHMARKS_HTM_HTMBENCH_DIR")/gem5-libs" ]] ; then
+        local GEM5_LIBS_DIR="${GEM5_ROOT}/gem5_path/benchmarks/libs/"
+        ln -s "$(realpath --relative-to="$(absolute_path "${BENCHMARKS_HTM_HTMBENCH_DIR}")" "$GEM5_LIBS_DIR")" "$(absolute_path "${BENCHMARKS_HTM_HTMBENCH_DIR}")/gem5-libs"
     fi
 }
 
